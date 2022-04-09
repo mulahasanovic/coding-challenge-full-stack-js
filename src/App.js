ff69c7ed4cc7497875da1e7d6bff6e7f0bfeed64
mulahasanovic <sead.m.bug@gmail.com>
2022-04-09 13:42:41 +0200

    Add simple axios hook and search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import {  useState } from "react";
+import useAxios from 'axios-hooks';
+
 function App() {
+  const [search, setSearch] = useState('');
+  const [tags, setTags] = useState('');
+
+  const [{ data, loading, error }, refetch] = useAxios(`http://localhost:3001/api/search?tags=${tags}`);
+
   return (
     <div>
-      Hello world!
+      <input type="search" value={search} onChange={e => setSearch(e.target.value)} />
+      <button onClick={() => {
+        setTags(search);
+        refetch();
+      }}>Search</button>
+      <hr />
+      {
+        loading ? 'loading...' : 'done'
+      }
+      <hr />
+      {
+      error
+      ? 'error'
+      : data?.map(item => (
+        <div key={item.id}>
+          <h3>{item.title}</h3>
+          <p>{item.tags}</p>
+          <img src={item.media.m} alt={item.title}/>
+        </div>
+        )
+      ) || []
+    }
     </div>
   );
 }
